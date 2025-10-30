cabfc54768729ce511bc9704a16113fcc1a240f5
mason0214 <masonleone13@gmail.com>
2025-10-30 09:52:37 -0400

    Wk 11 work

--- chapters/05_2_homework/rps.py
+++ chapters/05_2_homework/rps.py
@@ -1,22 +1,39 @@
 import getpass
 import pwinput
-player1 = pwinput.pwinput(prompt="Player 1: rock, paper, or scissors? ",mask='*')  
-player2 = pwinput.pwinput(prompt="Player 2: rock, paper, or scissors? ",mask='*')
 
-if (player1 == 'r' and player2 == 's'):
-    print('Player 1 wins.')
-elif (player1 == 's' and player2 == 'p'):
-    print('Player 1 wins.')
-elif (player1 == 'p' and player2 == 'r'):
-    print('Player 1 wins.')
-elif player1 == player2:
-    print("It's a tie!")
-elif (player1 == 'r' and player2 == 'p'):
-    print('Player 2 wins.')
-elif (player1 == 's' and player2 == 'r'):
-    print('Player 2 wins.')
-elif (player1 == 'p' and player2 == 's'):
-    print('Player 2 wins.')
-        
-else:
-    print('Someone did not enter a valid choice. Please use "r", "p", or "s".')
+
+
+
+
+def play_rps():
+    player1 = pwinput.pwinput(prompt="Player 1❓❓❓ ",mask='⚪')  
+    player2 = pwinput.pwinput(prompt="Player 2❓❓❓ ",mask='⚪')
+
+    if (player1 == 'r' and player2 == 's'):
+        print('😊Player 1 wins.😊   ⬅⬅⬅⬅⬅⬅⬅⬅⬅⬅')
+    elif (player1 == 's' and player2 == 'p'):
+        print('😊Player 1 wins.😊   ⬅⬅⬅⬅⬅⬅⬅⬅⬅⬅')
+    elif (player1 == 'p' and player2 == 'r'):
+        print('😊Player 1 wins.😊   ⬅⬅⬅⬅⬅⬅⬅⬅⬅⬅')
+    elif player1 == player2:
+        print("It's a tie! 👍   ⬅⬅⬅⬅⬅⬅⬅⬅⬅⬅")
+    elif (player1 == 'r' and player2 == 'p'):
+        print('😊Player 2 wins.😊   ⬅⬅⬅⬅⬅⬅⬅⬅⬅⬅')
+    elif (player1 == 's' and player2 == 'r'):
+        print('😊Player 2 wins.😊   ⬅⬅⬅⬅⬅⬅⬅⬅⬅⬅')
+    elif (player1 == 'p' and player2 == 's'):
+        print('😊Player 2 wins.😊   ⬅⬅⬅⬅⬅⬅⬅⬅⬅')
+            
+    else:
+        print('😔Please enter valid choice: r, p, or s😔')
+
+
+answer="Y"
+while answer == 'Y' or answer== 'y':
+    play_rps()
+    answer=input ('Would you like to play again?')
+
+
+    
+
+
