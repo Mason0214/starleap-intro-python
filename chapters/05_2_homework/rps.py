05e7890ef05ea63de2f35f0a1e85390243047b0d
mason0214 <masonleone13@gmail.com>
2025-11-06 09:51:05 -0500

    wk 12

--- chapters/05_2_homework/rps.py
+++ chapters/05_2_homework/rps.py
@@ -10,28 +10,32 @@ def play_rps():
     player2 = pwinput.pwinput(prompt="Player 2❓❓❓ ",mask='⚪')
 
     if (player1 == 'r' and player2 == 's'):
-        print('😊Player 1 wins.😊   ⬅⬅⬅⬅⬅⬅⬅⬅⬅⬅')
+        print('Player 1 wins.   ⬅⬅⬅⬅⬅⬅⬅⬅⬅⬅')
     elif (player1 == 's' and player2 == 'p'):
-        print('😊Player 1 wins.😊   ⬅⬅⬅⬅⬅⬅⬅⬅⬅⬅')
+        print('Player 1 wins.   ⬅⬅⬅⬅⬅⬅⬅⬅⬅⬅')
     elif (player1 == 'p' and player2 == 'r'):
-        print('😊Player 1 wins.😊   ⬅⬅⬅⬅⬅⬅⬅⬅⬅⬅')
+        print('Player 1 wins.   ⬅⬅⬅⬅⬅⬅⬅⬅⬅⬅')
     elif player1 == player2:
         print("It's a tie! 👍   ⬅⬅⬅⬅⬅⬅⬅⬅⬅⬅")
     elif (player1 == 'r' and player2 == 'p'):
-        print('😊Player 2 wins.😊   ⬅⬅⬅⬅⬅⬅⬅⬅⬅⬅')
+        print('Player 2 wins.  ⬅⬅⬅⬅⬅⬅⬅⬅⬅⬅')
     elif (player1 == 's' and player2 == 'r'):
-        print('😊Player 2 wins.😊   ⬅⬅⬅⬅⬅⬅⬅⬅⬅⬅')
+        print('Player 2 wins.   ⬅⬅⬅⬅⬅⬅⬅⬅⬅⬅')
     elif (player1 == 'p' and player2 == 's'):
-        print('😊Player 2 wins.😊   ⬅⬅⬅⬅⬅⬅⬅⬅⬅')
+        print('Player 2 wins.  ⬅⬅⬅⬅⬅⬅⬅⬅⬅')
             
     else:
-        print('😔Please enter valid choice: r, p, or s😔')
+        print('Please enter valid choice: r, p, or s')
 
 
 answer="Y"
+
 while answer == 'Y' or answer== 'y':
     play_rps()
-    answer=input ('Would you like to play again?')
+    answer=input ('Would you like to play again? [Y or N]')
+    
+while answer == 'N' or answer== 'n':
+    answer=input ('Ok.')
 
 
     
